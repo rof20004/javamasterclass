1505685d12e5c7c18be9b085c8767ce39ed40bbf
Rodolfo do Nascimento Azevedo <rodolfo@prodam.am.gov.br>
2020-01-06 17:15:44 -0400

    Initial commit

---
+++ section3-first-steps/src/main/java/helloworldchallengecommonerrors/Hello.java
@@ -0,0 +1,12 @@
+package helloworldchallengecommonerrors;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Rodolfo");
+		
+		int myFirstNumber = (10 + 5) + (2 * 10);
+		System.out.println(myFirstNumber);
+	}
+	
+}
