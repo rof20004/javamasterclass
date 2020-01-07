e07953d1fed1d52e7e2366d01cfaca6a7e7c860f
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2020-01-07 00:53:36 -0400

    Lessons added

--- section3-first-steps/src/main/java/variables/Hello.java
+++ section3-first-steps/src/main/java/variables/Hello.java
@@ -1,9 +1,12 @@
-package helloworldchallengecommonerrors;
+package variables;
 
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Rodolfo");
+
+		int myFirstNumber = (10 + 5) + (2 * 10);
+		System.out.println(myFirstNumber);
 	}
 
 }
